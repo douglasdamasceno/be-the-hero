d5584f487f869cd98aac30fb1f3f17a5b2047a67
douglasdm <douglasmdamasceno@gmail.com>
2020-05-03 11:48:06 -0300

    mobile finalizado

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const { errors } = require("celebrate");
+
 const router = require("./router");
 const app = express();
 app.use(cors());
+if (process.env.NODE_ENV !== "production") require("dotenv").load;
+
 app.use(express.json());
 app.use(router);
-app.listen(3333);
+app.use(errors());
+
+app.listen(process.env.PORT || 3333);
--- backend/src/router.js
+++ backend/src/router.js
@@ -1,4 +1,6 @@
 const router = require("express").Router();
+const { celebrate, Segments, Joi } = require("celebrate");
+
 const ongController = require("./controllers/OngController");
 const incidentController = require("./controllers/IncidentController");
 const ProfileController = require("./controllers/ProfileController");
@@ -7,12 +9,54 @@ const SessionController = require("./controllers/SessionController");
 router.post("/sessions", SessionController.create);
 
 router.get("/ongs", ongController.index);
-router.post("/ongs", ongController.create);
+router.post(
+  "/ongs",
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string()
+        .required()
+        .email(),
+      whatsapp: Joi.string()
+        .required()
+        .length(10),
+      city: Joi.string().required(),
+      uf: Joi.string()
+        .required()
+        .length(2)
+    })
+  }),
+  ongController.create
+);
 
-router.get("/profile", ProfileController.index);
+router.get(
+  "/profile",
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required()
+    }).unknown()
+  }),
+  ProfileController.index
+);
 
 router.post("/incidents", incidentController.create);
-router.get("/incidents", incidentController.index);
-router.delete("/incidents/:id", incidentController.delete);
+router.get(
+  "/incidents",
+  celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+      page: Joi.number()
+    })
+  }),
+  incidentController.index
+);
+router.delete(
+  "/incidents/:id",
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number().required()
+    })
+  }),
+  incidentController.delete
+);
 
 module.exports = router;
